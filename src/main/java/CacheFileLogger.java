5383ca30663ce77737d2ea91a31322ee203fafcd
Anton Platunov <Anton_Platunov@epam.com>
2019-05-21 15:25:06 +0300

    created class for collecting Evvents

---
+++ src/main/java/CacheFileLogger.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CacheFileLogger extends FileEventLogger {
+    private int cacheSize;
+    private List<Event> cache;
+    private FileEventLogger fileEventLogger;
+
+    public CacheFileLogger(String filename,int cacheSize) {
+        super(filename);
+        this.cacheSize = cacheSize;
+        this.cache = new ArrayList<Event>(cacheSize);
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        cache.add(event);
+
+        if (cache.size() == cacheSize) {
+            writeEventsFromCache();
+            cache.clear();
+        }
+    }
+
+    public void writeEventsFromCache() {
+        for (Event event : cache) {
+            fileEventLogger.logEvent(event);
+        }
+    }
+
+    public void destroy(){
+        if (!cache.isEmpty()){
+            writeEventsFromCache();
+        }
+    }
+}
